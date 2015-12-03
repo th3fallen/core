f3254e311c52be04530e43a3a2541f32655ceac1
Robin Appelman <icewind@owncloud.com>
2015-12-03 15:29:42 +0100

    Check that the owner of a link share still has share permissions on access

--- apps/dav/lib/connector/sabre/serverfactory.php
+++ apps/dav/lib/connector/sabre/serverfactory.php
@@ -121,7 +121,7 @@ class ServerFactory {
 			\OC::$server->getUserFolder();
 			
 			/** @var \OC\Files\View $view */
-			$view = $viewCallBack();
+			$view = $viewCallBack($server);
 			$rootInfo = $view->getFileInfo('');
 
 			// Create ownCloud Dir
